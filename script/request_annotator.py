894826952445560defaf510aff976b9f8a16c8a6
Piratevax <25346029+piratevax@users.noreply.github.com>
2020-05-22 11:34:05 +0200

    Annotatr request to arff

--- script/request_annotator.py
+++ script/request_annotator.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-import sys, argparse
+import sys, argparse, re
 import requests
+import urllib.request, urllib.error, urllib.parse
+from lxml import etree
+import json
+from pprint import pprint
 
 URL = "http://data.bioontology.org/annotator?"
 DEBUG = False
@@ -10,7 +14,7 @@ def arg_parser():
     parser = argparse.ArgumentParser(description = "Execute REST request to annotator ( https://bioportal.bioontology.org/annotator ) pubmed abstract.")
     semanticGroup = parser.add_argument_group()
     parser.add_argument("input", help = "input file name from pubmed (XML format)")
-    parser.add_argument("output", help = "output file name")
+    parser.add_argument("-o", "--output", help = "output file name", default = "output.arff")
     parser.add_argument("apikey", help = "mandatory key to access and use the REST API")
     parser.add_argument("--ontologies", help = "ontologies ID(s)", nargs = "+")
     semanticGroup.add_argument("--semantic-types", help = "semantic type(s)", nargs = "+")
@@ -27,7 +31,7 @@ def arg_parser():
     parser.add_argument("-d", "--debug", help = "execute the script in DEBUG mode", action = "store_true")
     return parser.parse_args()
 
-def request(apikey, text, format = 'xml', **options):
+def request(apikey, text, format = 'json', **options):
     ''' Parameters
 
         Filtering & query behavior
@@ -56,14 +60,137 @@ def request(apikey, text, format = 'xml', **options):
     response = requests.post(URL, data = data)
     if DEBUG:
         print(response.text)
-    return response.text
+    return json.loads(response.text)
 
+def get_request_list(filename):
+    tree = etree.parse(filename)
+    xpath = "//Article"
+    title_xpath = "ArticleTitle"
+    abstract_xpath = "Abstract/AbstractText"
+    request_list = []
+
+    for article in tree.xpath(xpath):
+        req = ""
+        req += article.findtext(title_xpath)
+        try:
+            req += article.findtext(abstract_xpath)
+        except TypeError:
+            request_list.append(req)
+            continue
+        request_list.append(req)            
+
+    return request_list
+
+def print_annotations(annotations, apikey, get_class=True):
+    for result in annotations:
+        class_details = result["annotatedClass"]
+        if get_class:
+            try:
+                class_details = get_json(result["annotatedClass"]["links"]["self"], apikey)
+            except urllib.error.HTTPError:
+                print(f"Error retrieving {result['annotatedClass']['@id']}")
+                continue
+        print("Class details")
+        print("\tid: " + class_details["@id"])
+        print("\tprefLabel: " + class_details["prefLabel"])
+        print("\tontology: " + class_details["links"]["ontology"])
+
+        print("Annotation details")
+        for annotation in result["annotations"]:
+            print("\tfrom: " + str(annotation["from"]))
+            print("\tto: " + str(annotation["to"]))
+            print("\tmatch type: " + annotation["matchType"])
+
+        if result["hierarchy"]:
+            print("\n\tHierarchy annotations")
+            for annotation in result["hierarchy"]:
+                try:
+                    class_details = get_json(annotation["annotatedClass"]["links"]["self"], apikey)
+                except urllib.error.HTTPError:
+                    print(f"Error retrieving {annotation['annotatedClass']['@id']}")
+                    continue
+                pref_label = class_details["prefLabel"] or "no label"
+                print("\t\tClass details")
+                print("\t\t\tid: " + class_details["@id"])
+                print("\t\t\tprefLabel: " + class_details["prefLabel"])
+                print("\t\t\tontology: " + class_details["links"]["ontology"])
+                print("\t\t\tdistance from originally annotated class: " + str(annotation["distance"]))
+        print("\n\n")
+
+def get_json(url, apikey):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('Authorization', 'apikey token=' + apikey)]
+    return json.loads(opener.open(url).read())
+
+def request_list(args, requests):
+    annotations = []
+
+    for req in requests:
+        annotation = request(args.apikey, req, longest_only = 'true')
+        # print(annotation)
+        # print_annotations(annotation, args.apikey)
+        annotations.append(annotation)
+
+    return annotations
+
+def get_words_list(annotations, apikey, get_class = True):
+    words_list = []
+
+    for annotation in annotations:
+        words = []
+        for result in annotation:
+            for anno in result["annotations"]:
+                word = anno["text"].lower()
+                word = "-".join(re.split("[' ]", word))
+                if word not in words:
+                    words.append(word)
+        words_list.append(words)
+
+    return words_list
+
+def write_arff(words_list, sep = ",", eol = "\n", filename = "output.arff"):
+    arff_to_write = "@relation annotator{}".format(eol)
+    flatten_unique_words_list = list(set([y for x in words_list for y in x]))
+    
+    print("Writing file")
+    arff_to_write += "@attribute class {{{}}}".format(sep.join(str(x) for x in range(1, len(words_list) + 1)))
+    arff_to_write += eol
+    arff_to_write += "@attribute annot {{{}}}".format(sep.join(x for x in flatten_unique_words_list))
+    arff_to_write += eol
+    arff_to_write += "@attribute bool {{true{} false}}{}".format(sep, eol)
+    arff_to_write += "@data{}".format(sep)
+
+    ind = 1
+    for x in words_list:
+        for word in flatten_unique_words_list:
+            arff_to_write += "{}{}{}{}".format(ind, sep, word, sep)
+            if word in x:
+                arff_to_write += "true{}".format(eol)
+            else:
+                arff_to_write += "false{}".format(eol)
+        ind += 1
+    arff_to_write += eol
+
+    fd = open(filename, "w")
+    fd.write(arff_to_write)
+    fd.close()
+    print(" file wrote : {}".format(filename))
 
 def main():
     global DEBUG
     args = arg_parser()
     DEBUG = args.debug
-    request(args.apikey, "Amyotrophic lateral sclerosis (ALS) is a progressive and fatal neurodegenerative disorder of the human motor system. Neuroinflammation appears to be an important modulator of disease progression in ALS. Specifically, reduction of regulatory T cell (Treg) levels, along with an increase in pro-inflammatory effector T cells, macrophage activation and upregulation of co-stimulatory pathways have all been associated with a rapid disease course in ALS. Autologous infusion of expanded Tregs into sporadic ALS patients, resulted in greater suppressive function, slowing of disease progression and stabilization of respiratory function. Tecfidera (dimethyl fumarate) increases the ratio of anti-inflammatory (Treg) to proinflammatory T-cells in patients with relapsing remitting multiple sclerosis and rebalances the regulatory: inflammatory axis towards a neuroprotective phenotype. Consequently, the aim of this study was to assess the efficacy, safety, and tolerability of Tecfidera in sporadic ALS.", ontologies = 'MESH', longest_only = 'true')
+
+    print("Formatting requests")
+    requests = get_request_list(args.input)
+    print("Requesting Annotator")
+    annotations = request_list(args, requests)
+    print("Formatting Annotator results")
+    words_list = get_words_list(annotations, args.apikey)
+    write_arff(words_list, filename = args.output)
+
+    # answer = request(args.apikey, "Amyotrophic lateral sclerosis (ALS) is a progressive and fatal neurodegenerative disorder of the human motor system. Neuroinflammation appears to be an important modulator of disease progression in ALS. Specifically, reduction of regulatory T cell (Treg) levels, along with an increase in pro-inflammatory effector T cells, macrophage activation and upregulation of co-stimulatory pathways have all been associated with a rapid disease course in ALS. Autologous infusion of expanded Tregs into sporadic ALS patients, resulted in greater suppressive function, slowing of disease progression and stabilization of respiratory function. Tecfidera (dimethyl fumarate) increases the ratio of anti-inflammatory (Treg) to proinflammatory T-cells in patients with relapsing remitting multiple sclerosis and rebalances the regulatory: inflammatory axis towards a neuroprotective phenotype. Consequently, the aim of this study was to assess the efficacy, safety, and tolerability of Tecfidera in sporadic ALS.", ontologies = 'MESH', longest_only = 'true')
+    # print(answer)
 
 if __name__ == "__main__":
     main()
